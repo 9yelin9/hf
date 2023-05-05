7807cdd935ceee102e1207751215ce31ea91fea1
9yelin9 <9yelin9@gmail.com>
2023-05-06 03:09:19 +0900

    Fix

--- pyhf/com.py
+++ pyhf/com.py
@@ -26,14 +26,14 @@ def ReadConfig(dim):
 def FnDict(fn):
 	fn_dict = {
 		'type':  re.sub('_', '', re.search('[A-Z]\d_', fn).group()),
-		'JU':    float(re.sub('_JU',    '', re.search('_JU\d+[.]\d+',        fn).group())),
-		'N':     float(re.sub('_N',     '', re.search('_N\d+[.]\d+',         fn).group())),
+		'JU':    float(re.sub('JU',    '', re.search('JU\d+[.]\d+',        fn).group())),
+		'N':     float(re.sub('N_',     '', re.search('N_\d+[.]\d+',         fn).group())),
 		'U':     float(re.sub('_U',     '', re.search('_U\d+[.]\d+',         fn).group())),
 		'n':     float(re.sub('_n',     '', re.search('_n\d+[.]\d+',         fn).group())),
 		'm':     float(re.sub('_m',     '', re.search('_m[-]?\d+[.]\d+',     fn).group())),
 		'e':     float(re.sub('_e',     '', re.search('_e[-]?\d+[.]\d+',     fn).group())),
-		'fermi': float(re.sub('_fermi', '', re.search('_fermi[-]?\d+[.]\d+', fn).group())),
-		'gap':   float(re.sub('_gap',   '', re.search('_gap[-]?\d+[.]\d+',   fn).group())),
+		'fermi': float(re.sub('fermi', '', re.search('fermi[-]?\d+[.]\d+', fn).group())),
+		'gap':   float(re.sub('gap',   '', re.search('gap[-]?\d+[.]\d+',   fn).group())),
 	}
 
 	return fn_dict
