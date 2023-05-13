268f15a22fec558c19940e02fab3dcccbd290fad
9yelin9 <9yelin9@gmail.com>
2023-05-13 23:18:00 +0900

    Fix

--- pyhf/outhf.py
+++ pyhf/outhf.py
@@ -280,7 +280,7 @@ class OutHF:
 		#ct = ax.contour(N, U, m, levels=[tol_m], colors='w', linestyles='dotted')
 		#if abs(tol_m - 0.1) < 1e-6: ax.clabel(ct, ct.levels, inline=True, fmt='%.1f', fontsize=16)
 
-		cf = ax.contourf(X, Y, Z, levels=np.linspace(0.1, self.Nb//2, Nlevel), cmap='summer')
+		cf = ax.contourf(X, Y, Z, levels=np.linspace(0.1, self.Nb//2, Nlevel), cmap='plasma')
 		cb = plt.colorbar(cf, format='%.1f')
 		cb.set_ticks([0.1, self.Nb//2])
 		cb.set_ticklabels(['0.1', '3.0'])
